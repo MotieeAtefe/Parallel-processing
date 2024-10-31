270288d16f6c952861838019d85982bd1dc0a79d
MotieeAtefe <motiee1377@gmaile.com>
2024-10-31 18:44:02 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Thread
+{
+    internal class Program
+    {
+      
+        static List<Students> GetStudentData() =>
+            Enumerable.Range(1,10).Select(i => new Students
+            {
+                    Id = i,
+                    Name = $"Student-{i}"
+            }).ToList();
+        static async Task<int> ProcessStudentAsync(Students student)
+        {
+            await Task.Delay(100);
+            Console.WriteLine($"Processing data for {student.Name}");
+            return student.Id * 2;
+        }
+        static async Task Main()
+        {
+            var students = GetStudentData();
+        }
+
+    }
+}
